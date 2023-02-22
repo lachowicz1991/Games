f91d9ff2033cde06a7c4f52352f7eeb09c36bb47
lachowicz1991 <lachowicz1991@protonmail.com>
2023-02-22 16:14:08 +0100

    refactoring - oop added

--- main.py
+++ main.py
@@ -1,49 +1,25 @@
-"""
-This is the Template Repl for Python with Turtle.
-
-Python with Turtle lets you make graphics easily in Python.
-
-Check out the official docs here: https://docs.python.org/3/library/turtle.html
-"""
-import colorgram
-import turtle
-import random as rd
-rgb_colors = []
-colors = colorgram.extract(
-    'dot.jpg', 100)
-t = turtle.Turtle()
-f_color = colors[0]
-rgb = f_color.rgb
+from snake import Snake
+from turtle import Screen
+from time import sleep
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('My Snake Game')
+screen.tracer(0)
 
 
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up,'Up')
+screen.onkey(snake.down,'Down')
+screen.onkey(snake.left,'Left')
+screen.onkey(snake.right,'Right')
+game_is_on = True
+while game_is_on:
+    screen.update()
+    sleep(0.1)
+    snake.move()
 
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r,g,b)
-    rgb_colors.append(new_color)
 
-print(rgb_colors)
 
-turtle.colormode(255)
-t.speed('fastest')
-t.penup()
-t.hideturtle()
-t.setheading(225)
-t.forward(300)
-t.setheading(0)
-num_of_dots = 101
-for x in range(1,  num_of_dots):
-    t.dot(20, rd.choice(rgb_colors))
-    t.forward(50)
-    if x % 10 == 0:
-        t.setheading(90)
-        t.forward(50)
-        t.setheading(180)
-        t.forward(500)
-        t.setheading(0)
-
-screen = turtle.Screen()
 screen.exitonclick()
-
--- snake.py
+++ snake.py
@@ -1,19 +1,58 @@
 from turtle import Screen, Turtle
+from time import sleep
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor('black')
-screen.title('My Snake Game')
+START_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in START_POSITION:
+            t = Turtle(shape='square')
+            t.penup()
+            t.color('white')
+            t.goto(position)
+            t.penup()
+            self.segments.append(t)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
 
 
-turtle_gang = []
-y_position = 0
-for turtle_index in range(0, 3):
-    t = Turtle(shape='square')
-    t.penup()
-    t.goto(x=y_position, y=0)
-    t.color('white')
-    turtle_gang.append(t)
-    y_position += 20
 
-screen.exitonclick()
+
+
+
+
+
+
+
+
